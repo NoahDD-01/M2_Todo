b02a359371d13f770582c38b167a6d786eb3265e
NoahDD-01 <noahdd216@gmail.com>
2025-05-15 15:27:49 +0630

    todo_update

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class TodoappModel(models.Model):
+    task = models.CharField(max_length=200)
+    note = models.TextField()
+    complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now_add=True)
